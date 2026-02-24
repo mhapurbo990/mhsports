cfeb8f38c349e70d2c14057fed4644fffb78d7da
mhapurbo990 <139166114+mhapurbo990@users.noreply.github.com>
2026-02-25 00:24:29 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,26 +1,13 @@
 from flask import Flask, render_template
-import requests
 
 app = Flask(__name__, template_folder='../templates')
 
 @app.route('/')
 def home():
-    url = "https://raw.githubusercontent.com/sabbiriptv/CricHD/main/crichd.m3u"
-    channels = []
-    try:
-        r = requests.get(url, timeout=10)
-        if r.status_code == 200:
-            lines = r.text.split('\n')
-            for i in range(len(lines)):
-                if lines[i].startswith('#EXTINF'):
-                    name = lines[i].split(',')[-1].strip()
-                    logo = ""
-                    if 'tvg-logo="' in lines[i]:
-                        logo = lines[i].split('tvg-logo="')[1].split('"')[0]
-                    
-                    stream_link = lines[i+1].strip() if (i+1) < len(lines) else "#"
-                    if name and stream_link.startswith('http'):
-                        channels.append({"title": name, "logo": logo, "url": stream_link})
-    except:
-        pass
-    return render_template('index.html', channels=channels)
+    # Ami nijei 3-ta channel manually pathachchi check korar jonno
+    test_channels = [
+        {"title": "PTV Sports", "logo": "https://i.imgur.com/82Nt8Dw.jpg", "url": "http://62.210.138.11:8000/play/ptvsports/index.m3u8"},
+        {"title": "Star Sports 1", "logo": "https://i.imgur.com/NU0XdZv.jpg", "url": "http://62.210.138.11:8000/play/star1/index.m3u8"},
+        {"title": "Willow HD", "logo": "https://i.imgur.com/TmzGuSD.jpg", "url": "http://62.210.138.11:8000/play/willow/index.m3u8"}
+    ]
+    return render_template('index.html', channels=test_channels)
